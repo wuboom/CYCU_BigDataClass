22cca91caff3b2517bca13a77659937eede491d7
wuboom <wubocong@yahoo.com>
2019-11-23 07:41:15 +0000

    change run.sh website to mywebsite

--- hadoop_multi_nodes/run.sh
+++ hadoop_multi_nodes/run.sh
@@ -12,7 +12,7 @@ sudo chown -R hduser:hadoop /usr/local/hadoop-2.10.0/
 sudo chown -R root:root /usr/local/jdk1.8.0_231
 
 
-sudo git clone https://github.com/fychao/CYCU_BigDataClass.git /usr/local/hadoop/etc/CYCU_BigDataClass
+sudo git clone https://github.com/wuboom/CYCU_BigDataClass /usr/local/hadoop/etc/CYCU_BigDataClass
 sudo chown -R hduser:hadoop /usr/local/hadoop/etc/CYCU_BigDataClass
 sudo mv /usr/local/hadoop/etc/hadoop /usr/local/hadoop/etc/hadoop_bak
 sudo ln -s /usr/local/hadoop/etc/CYCU_BigDataClass/hadoop_multi_nodes/hadoop /usr/local/hadoop/etc/hadoop
