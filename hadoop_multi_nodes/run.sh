0cf9f0964f831505b05453deb27a7386a242d5df
AUGUST CHAO <fychao.tw@gmail.com>
2019-11-23 17:21:46 +0800

    Update run.sh

--- hadoop_multi_nodes/run.sh
+++ hadoop_multi_nodes/run.sh
@@ -1,3 +1,7 @@
+HADOOP_HOME=/usr/local/hadoop
+HADOOP_HOME_SBIN=$HADOOP_HOME/sbin
+HADOOP_HOME_BIN=$HADOOP_HOME/bin
+
 # 中原巨資 上課實驗用
 sudo addgroup hadoop  
 sudo adduser --disabled-password  --gecos "" --ingroup hadoop hduser
@@ -7,16 +11,16 @@ wget http://172.18.39.68/os/hadoop-2.10.0.tar.gz
 
 sudo tar zxvf hadoop-2.10.0.tar.gz -C /usr/local/
 sudo tar zxvf jdk-8u231-linux-x64.tar.gz -C /usr/local/
-sudo ln -s /usr/local/hadoop-2.10.0/ /usr/local/hadoop
+sudo ln -s /usr/local/hadoop-2.10.0/ $HADOOP_HOME
 sudo ln -s /usr/local/jdk1.8.0_231/ /usr/local/jdk
 sudo chown -R hduser:hadoop /usr/local/hadoop-2.10.0/
 sudo chown -R root:root /usr/local/jdk1.8.0_231
 
 
-sudo git clone  https://github.com/fychao/CYCU_BigDataClass.git /usr/local/hadoop/etc/CYCU_BigDataClass
-sudo chown -R hduser:hadoop /usr/local/hadoop/etc/CYCU_BigDataClass
-sudo mv /usr/local/hadoop/etc/hadoop /usr/local/hadoop/etc/hadoop_bak
-sudo ln -s /usr/local/hadoop/etc/CYCU_BigDataClass/hadoop_multi_nodes/hadoop /usr/local/hadoop/etc/hadoop
+sudo git clone  https://github.com/fychao/CYCU_BigDataClass.git $HADOOP_HOME/etc/CYCU_BigDataClass
+sudo chown -R hduser:hadoop $HADOOP_HOME/etc/CYCU_BigDataClass
+sudo mv $HADOOP_HOME/etc/hadoop $HADOOP_HOME/etc/hadoop_bak
+sudo ln -s $HADOOP_HOME/etc/CYCU_BigDataClass/hadoop_multi_nodes/hadoop $HADOOP_HOME/hadoop/etc/hadoop
 
 HADOOP_HOME=/usr/local/hadoop
 HADOOP_HOME_SBIN=$HADOOP_HOME/sbin
